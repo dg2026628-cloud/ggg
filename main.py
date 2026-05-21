77c9dc247fdaf65a0ef1dee922fe3adcea234459
dg2026628-cloud <dg2026628@danggok.hs.kr>
2026-05-21 09:05:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 Web VPython 3.2
-import random
-b = box(make_trail = True)
+ball = sphere(pos=vector(0, 0, 0), radius=0.5, color=color.gray)
+ball = sphere(pos=vector(0.5, 0.3, 0), radius=0.3, color=color.gray)
+ball = sphere(pos=vector(-0.5, 0.3, 0), radius=0.3, color=color.gray)
+ball = sphere(pos=vector(0.2, 0.1, 0.4), radius=0.05, color=color.black)
+ball = sphere(pos=vector(-0.2, 0.1, 0.4), radius=0.05, color=color.black)
+ball = sphere(pos=vector(0.0, 0.1, 0.5), radius=0.03, color=color.black)
 
-while True : 
-    rate(100)
-    k = keysdown()
-    if ' ' in k :
-        b.pos.x = random.uniform(-5, 5)
-        b.pos.y = random.uniform(-5, 5)
-        b.color = vec(random.uniform(0.9,1),random.random(),random.random())
-        b.trail_color = b.
